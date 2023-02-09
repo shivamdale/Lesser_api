7e3a778858d1cc60ab2c708b4d1ffaac7e6f4ded
yogeshwar.yaduwanshi@taskcraft.in <yogeshwar.yaduwanshi@taskcraft.in>
2023-02-09 09:47:42 +0000

    [952768] Email Validation Feature

--- template-app/app/models/account_block/sms_account.rb
+++ template-app/app/models/account_block/sms_account.rb
@@ -2,5 +2,6 @@ module AccountBlock
   class SmsAccount < Account
     include Wisper::Publisher
     validates :full_phone_number, uniqueness: true, presence: true
+    validates :email, uniqueness: true, presence: true
   end
 end
